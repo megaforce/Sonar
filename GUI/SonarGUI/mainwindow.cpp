1667965a1467e22209c4245f2cf6f1a47a27a59b
megaforce <matjaz.zibert2@gmail.com>
2018-03-28 19:41:29 +0200

    Basic serial communication
    Made a basic serial communication interface for arduino mega

---
+++ GUI/SonarGUI/mainwindow.cpp
@@ -0,0 +1,50 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    arduino_is_available = false;
+    arduino_port_name = "";
+
+    arduino = new QSerialPort;
+
+    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
+            if(serialPortInfo.vendorIdentifier() == arduino_mega_vendor_id){
+                if(serialPortInfo.productIdentifier() == arduino_mega_product_id){
+                    arduino_port_name = serialPortInfo.portName();
+                    arduino_is_available = true;
+                }
+            }
+        }
+    }
+
+    if(arduino_is_available){
+
+        arduino->setPortName(arduino_port_name);
+        arduino->open(QSerialPort::ReadOnly);
+        arduino->setBaudRate(QSerialPort::Baud9600);
+        arduino->setDataBits(QSerialPort::Data8);
+        arduino->setParity(QSerialPort::NoParity);
+        arduino->setStopBits(QSerialPort::OneStop);
+        arduino->setFlowControl(QSerialPort::NoFlowControl);
+
+    }
+    else{
+
+        QMessageBox::warning(this, "Port error", "Couldn't find the Arduino!");
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *e)
+{
+
+}
---
+++ GUI/SonarGUI/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMainWindow>
+#include <QtGui>
+#include <QtCore>
+#include <QDialog>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <Qserialport>
+#include <QSerialPortInfo>
+#include <QDebug>
+#include <QLinkedList>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
