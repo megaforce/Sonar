d2057b8c69944fca6d1102efd9763a1c9f670304
megaforce <matjaz.zibert2@gmail.com>
2018-03-29 18:52:19 +0200

    Improved serial communication

--- GUI/SonarGUI/mainwindow.cpp
+++ GUI/SonarGUI/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //this->setStyleSheet("background-color: black;");
     arduino_is_available = false;
     arduino_port_name = "";
 
@@ -26,7 +27,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
         arduino->setPortName(arduino_port_name);
         arduino->open(QSerialPort::ReadOnly);
-        arduino->setBaudRate(QSerialPort::Baud9600);
+        arduino->setBaudRate(QSerialPort::Baud4800);
         arduino->setDataBits(QSerialPort::Data8);
         arduino->setParity(QSerialPort::NoParity);
         arduino->setStopBits(QSerialPort::OneStop);
@@ -37,6 +38,10 @@ MainWindow::MainWindow(QWidget *parent) :
 
         QMessageBox::warning(this, "Port error", "Couldn't find the Arduino!");
     }
+
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(update_screen()));
+    timer->start(340);
 }
 
 MainWindow::~MainWindow()
@@ -44,7 +49,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::getSerialData()
+{
+    QByteArray serialDataRecieved;
+    serialDataRecieved=arduino->readAll();
+    if(serialDataRecieved.size() <5)
+    serialDataRecieved.clear();
+    else
+    {
+    QString sDR;
+    QStringList data;
+    sDR = serialDataRecieved;
+    data = sDR.split("|||");
+    data.removeAll("|");
+        if (data.size() == 2 and data[0].size() < 4 )
+        {
+        points.append(point{data[0].toInt(),data[1].toInt()});
+        }
+    }
+}
+
 void MainWindow::paintEvent(QPaintEvent *e)
 {
+    QPainter painter(this);
+    QPen pen( Qt::green );
+    pen.setWidth(5);
+    painter.setPen(pen);
 
+}
+
+void MainWindow::update_screen()
+{
+ getSerialData();
+ update();
 }
--- GUI/SonarGUI/mainwindow.h
+++ GUI/SonarGUI/mainwindow.h
@@ -12,6 +12,16 @@
 #include <QSerialPortInfo>
 #include <QDebug>
 #include <QLinkedList>
+#include <QMessageBox>
+#include <QTimer>
+#include <Qstring>
+#include <QList>
+
+struct point
+{
+    short angle;
+    short distance;
+};
 
 namespace Ui {
 class MainWindow;
@@ -26,7 +36,22 @@ public:
     ~MainWindow();
 
 private:
+    QList<point> points;
     Ui::MainWindow *ui;
+    QSerialPort *arduino;
+    static const quint16 arduino_mega_vendor_id = 9025;
+    static const quint16 arduino_mega_product_id = 66;
+    QString arduino_port_name;
+    bool arduino_is_available;
+    QTimer *timer;
+    void getSerialData();
+
+protected:
+  void paintEvent(QPaintEvent *e);
+
+private slots:
+  void update_screen();
+
 };
 
 #endif // MAINWINDOW_H
